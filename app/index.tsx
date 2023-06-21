f6a4b25bccbe319a5fea815d5682c96c4c593b7c
mayapony <mayapony@foxmail.com>
2023-06-21 14:02:27 +0800

    ✨ navigator bar immense

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,10 @@
+import { DARK_THEME } from "constants/theme";
+import * as NavigationBar from "expo-navigation-bar";
 import Home from "./Home";
 
-export default function Root() {
+export default function App() {
+  NavigationBar.setBackgroundColorAsync(DARK_THEME.base);
+  NavigationBar.setBorderColorAsync(DARK_THEME.base);
+
   return <Home />;
 }
