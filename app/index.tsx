95d013d88c37a1db3ab568297f7bed49844d671d
mayapony <mayapony@foxmail.com>
2023-06-27 13:15:30 +0800

    🔧 update expo app.json & eas.json

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,10 @@
 import { DARK_THEME } from "constants/theme";
-import * as NavigationBar from "expo-navigation-bar";
 import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native";
 import { styles } from "styles/home.style";
 import Home from "./Home";
 
 export default function App() {
-  NavigationBar.setBackgroundColorAsync(DARK_THEME.base);
-  NavigationBar.setBorderColorAsync(DARK_THEME.base);
-
   return (
     <SafeAreaView style={styles.homeArea}>
       <Stack.Screen
